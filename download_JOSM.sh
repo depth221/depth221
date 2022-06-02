3de4701d4e9a766d8bbd56ee532b16074a66d2f6
Dongha Hwang <depth221@gmail.com>
2022-06-02 17:54:08 +0900

    Update download_JOSM.sh

--- download_JOSM.sh
+++ download_JOSM.sh
@@ -6,8 +6,8 @@ mv preferences.xml .config/JOSM
 mkdir .local/share/JOSM
 wget https://josm.openstreetmap.de/osmsvn/applications/editors/josm/dist/buildings_tools.jar
 wget https://raw.githubusercontent.com/depth221/depth221/main/.local/share/JOSM/plugins/site-josm.openstreetmap.de-_pluginicons.txt
-mv buildings_tools.jar .local/share/JOSM
-mv "site-josm.openstreetmap.de-_pluginicons.txt" .local/share/JOSM
+mv buildings_tools.jar .local/share/JOSM/plugins
+mv "site-josm.openstreetmap.de-_pluginicons.txt" .local/share/JOSM/plugins
 
 wget https://download.oracle.com/java/18/archive/jdk-18.0.1.1_linux-x64_bin.tar.gz
 tar -zxvf jdk-18.0.1.1_linux-x64_bin.tar.gz
